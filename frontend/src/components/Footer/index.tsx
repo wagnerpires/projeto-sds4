8784308b4a4c912591f212dfd8058071105f66f1
Wagner Pires <wagnerpires@hotmail.com>
2021-09-06 20:44:09 -0300

    Projeto SDS 4.0 - frontend

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer">Wagner Pires</a></p>
+                <p className="text-light"><small><strong>WR - TECNOLOGIA EM SISTEMAS - (61)98162-6852</strong><br /></small></p>
+                <p className="text-light"><small><strong>Semana Spring React 4.0 - 6 a 12 de setembro/2021</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
