9ddce88ad1359fa5f7477e371dd0b9d51dcd0431
Wagner Dba <wagnerdba@gmail.com>
2023-02-20 14:00:00 -0300

    RUGE Capital

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,8 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer">Wagner Pires</a></p>
                 <p className="text-light"><small><strong>WR - TECNOLOGIA EM SISTEMAS - (61)98162-6852</strong><br /></small></p>
-                <p className="text-light"><small><strong>Semana Spring React 4.0 - 6 a 12 de setembro/2021</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light"><small><strong>wagnerdba@gmail.com</strong><br /></small></p>
             </div>
         </footer>
 
