255703e277d75d2175aabd6c963bcb78183265a9
Wagner Dba <wagnerdba@gmail.com>
2023-02-20 10:04:08 -0300

    testes

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-3">Painel de Vendas - Teste Gabriel</h1>
+                <h1 className="text-primary py-3">Painel de Vendas</h1>
 
                 <div className="row px-3">
                     <div className="col-sm-6">
